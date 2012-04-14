8a118ce9c521a1b8a20da56ae5ee3f15e3ab299c
Teh Whale <tehwhale@live.com>
2012-04-14 02:59:51 -0400

    Users and such are done, logging in, messages, friendly urls, etc.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,10 +4,10 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by_email(params[:session][:email])
-
 		if user && user.authenticate(params[:session][:password])
 			sign_in user
-			redirect_to user
+			flash[:success] = "You have successfully logged in"
+			redirect_back_or user
 		else
 			flash.now[:error] = 'Invalid email/password combination'
 			render 'new'
@@ -16,6 +16,7 @@ class SessionsController < ApplicationController
 
 	def destroy
 		sign_out
+		flash[:success] = "You have successfully logged out"
 		redirect_to root_path
 	end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,20 @@
 class UsersController < ApplicationController
+  before_filter :signed_in_user, only: [:index, :edit, :update]
+  before_filter :correct_user, only: [:edit, :update]
+
+  def index
+  	@users = User.all
+  end
+
   def show
   	@user = User.find(params[:id])
   end
 
   def new
   	@user = User.new
+  end
+
+  def edit
   end
 
   def create
@@ -17,4 +27,30 @@ class UsersController < ApplicationController
   		render 'new'
   	end
   end
+
+  def update
+  	if @user.update_attributes(params[:user])
+  		flash[:success] = "Profile updated"
+  		sign_in @user
+  		redirect_to @user
+  	else
+  		render 'edit'
+  	end
+  end
+
+  private
+  	def signed_in_user
+  		unless signed_in?
+  			store_location
+  			redirect_to signin_path, error: "You must first sign in to access that page."
+  		end
+  	end
+
+  	def correct_user
+  		@user = User.find(params[:id])
+  		unless current_user?(@user)
+  			flash[:error] = "YO BITCH, WHAT THE FUCK ARE YOU DOING?"
+  			redirect_to root_path
+  		end
+  	end
 end
